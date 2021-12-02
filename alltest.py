29835c62e37cb8557ca171e3b57839aac783a47e
szymonfelinski <szymon583@gmail.com>
2021-12-02 18:48:48 +0100

    Update alltest.py

--- alltest.py
+++ alltest.py
@@ -3,7 +3,7 @@
 gDebug = 0 #will the csv debug file be written?
 gLog = 1 #will the csv data file be written? (if 0, the program has no stop condition!)
 gDisplayData = 1 #display data on the display?
-gReverseGeocode = 0 #attempt to reverse geocode GPS data?
+gReverseGeocode = 1 #attempt to reverse geocode GPS data?
 gVerbose = 1 #verbose output in console? (does nothing atm - 06.09.21)
 stop_after_time_passed = 0 #will the program stop after the amount of time set in recording_time?
 gRecording_time = 1800 #time of recording in seconds
@@ -14,13 +14,10 @@ csv_file = []
 csv_file_debug = []
 writer = []
 writer_debug = []
-packet = []
-reverse_geocode_result = []
-d = [128, 64]
-
 geocode_process = []
 display_process = []
 main_process = []
+d = [128, 64]
 
 road = "unresolved"
 house = "unresolved"
@@ -28,7 +25,6 @@ house = "unresolved"
 import multiprocessing as mp
 
 import gpsd #gps library
-from OSMPythonTools.nominatim import Nominatim #maps library (reverse geocoding)
 import time
 from mpu6050 import mpu6050 #imu library
 import math
@@ -42,6 +38,12 @@ if gDisplayData:
     from luma.core import legacy
     from luma.oled.device import sh1106
     from luma.core.interface.serial import i2c
+
+if gReverseGeocode:
+    from OSMPythonTools.nominatim import Nominatim #maps library (reverse geocoding)
+    geocode_api=Nominatim() #binding reverse geocoding to a process variable
+    #reverse_geocode_result = geocode_api.query(40.714224, -73.961452, reverse=True)
+    #print(reverse_geocode_result.displayName())
     
 #global variables
 start_time=time.time() #save date and time of script start (seconds since epoch - UTC)
@@ -49,11 +51,7 @@ start_time=time.time() #save date and time of script start (seconds since epoch 
 mpu = mpu6050(0x68) #0x68 is device address on i2c bus
 
 # Connect to the local gpsd
-gpsd.connect() #this takes a while (MUST BE SET UP USING sudo gpsd /dev/serial0 -F /var/run/gpsd.sock !!! #EDIT 6/8/21 - added automatic execution at system startup.)
-
-geocode_api=Nominatim() #binding reverse geocoding to a process variable
-#reverse_geocode_result = geocode_api.query(40.714224, -73.961452, reverse=True)
-#print(reverse_geocode_result.displayName())
+gpsd.connect() #this takes a while (MUST BE SET UP USING sudo gpsd /dev/serial0 -F /var/run/gpsd.sock !!! #EDIT 06.08.21 - added automatic execution at system startup.)
 
 #DISPLAY
 MY_CUSTOM_BITMAP_FONT = [ # Note that "\0" is the zero-th character in the font
@@ -94,7 +92,7 @@ def loggingFuncInit(debug):
     if debug:
         writer_temp.writerow(['t','date_diff','new_date_diff','newsleep'])
     else:
-        writer_temp.writerow(['t','ax','ay','az','a_length','latitude','longitude','speed', 'Street Name', 'House Number']) #csv headers
+        writer_temp.writerow(['t','ax','ay','az','a_length','latitude','longitude','speed']) #csv headers - removed road and street - 06.09.21
     return csv_file, writer_temp
 
 #def debugFuncInit(): #old debug file init - removed 08.08.2021
@@ -108,8 +106,6 @@ def init():
     writer_init = None
     csv_file_debug_init = None
     writer_debug_init = None
-    
-    
     
     if gLog:
         try:
@@ -131,47 +127,92 @@ def init():
     csv_file_debug = csv_file_debug_init
     global writer_debug
     writer_debug = writer_debug_init
-    
-
 
 def gpsResolve():
     return gpsd.get_current()
     
 def reverseGeocode(sentGeo):
-    #global packet
     while True:
         try:
-            #takes a while the first time
-            temp = geocode_api.query(packet.lat, packet.lon, reverse=True, zoom=18)
-            sentGeo.send(temp)
+            packet = gpsResolve() #this works better than sending a packet from display.
+            geocodingResult = geocode_api.query(packet.lat, packet.lon, reverse=True, zoom=18) #takes a while the first time
+            if geocodingResult.isReverse(): #if the response is actual reverse geocode data
+                sentGeo.send(geocodingResult) #send data to pipe
+                #print("GEOCODE: Address found.")
+                #print(geocodingResult.address())
+        except KeyboardInterrupt: #this is important. without it, the display process would run indefinitely.
+            raise SystemExit
         except:
-            print("GEO: Can't resolve address")
+            print("GEOCODE: Can't resolve address")
+            
 
 
-def displayData(pipe):
+def displayData(receivedData):
+    global road
+    global house
     if gDisplayData:
         try:
             display = displayInit()
         except:
-            print("Couldn't init display")
+            print("DISPLAY: Couldn't init display")
+            
+    if gReverseGeocode: #TODO
+        receivedGeo, sentGeo = mp.Pipe()
+        geocode_process = mp.Process(target = reverseGeocode, args=((sentGeo),)) #added 08.09.21
+        print("DISPLAY: Starting reverse geocode process.")
+        geocode_process.start()
     
-    receivedData, sentData = pipe
-    sentData.close() #reading data only
-    
-    while True:
+    while True: #tutaj kurwo pisz wyświetlacz nie?
         try:
             while receivedData.poll(): #this small loop reads only the last item in the pipe (basically skips elements until none are left)
-                a_length, packet, road, house = receivedData.recv()
+                a_length, packet, cycle = receivedData.recv()
+        except KeyboardInterrupt: #this is important. without it, the display process would run indefinitely.
+            if gReverseGeocode:
+                geocode_process.terminate()
+            raise SystemExit
         except:
             print("DISPLAY: No data received")
         
+        #resolve reverse geocoded address
+        if gReverseGeocode:
+            try:
+                while receivedGeo.poll(): #only pull the latest result
+                    reverse_geocode_result = receivedGeo.recv()
+            except:
+                print("DISPLAY: Can't resolve address")
+        
+            oldroad=road
+            oldhouse=house
+            
+            try:
+                road = reverse_geocode_result.address()['road']
+
+            except: #fallback to old address if reverse geocoding failed (and quarter handling #10.09.21)
+                try:
+                    road = reverse_geocode_result.address()['quarter']
+                except: 
+                    road = oldroad
+                #print("DISPLAY: Old: "+road+" "+house)
+                
+            try:                
+                house = reverse_geocode_result.address()['house_number']
+                #print("DISPLAY: "+road+" "+house)
+            except: #fallback to old address if reverse geocoding failed
+                house = oldhouse
+        
         cur_time=time.time()
         cur_timestamp=time.localtime(cur_time)
-        with canvas(display) as draw: #execution time: around 100ms (good!)
-            if cur_timestamp.tm_min<10: #clock
-                legacy.text(draw, (0, 0), str(cur_timestamp.tm_hour)+":0"+str(cur_timestamp.tm_min), fill="white")
+        with canvas(display) as draw: #execution time: around 100ms (good!) #do tego momentu nic nie ruszaj ok?
+            if cur_timestamp.tm_sec%2:
+                if cur_timestamp.tm_min<10: #clock
+                    legacy.text(draw, (0, 0), str(cur_timestamp.tm_hour)+":0"+str(cur_timestamp.tm_min), fill="white")
+                else:
+                    legacy.text(draw, (0, 0), str(cur_timestamp.tm_hour)+":"+str(cur_timestamp.tm_min), fill="white")
             else:
-                legacy.text(draw, (0, 0), str(cur_timestamp.tm_hour)+":"+str(cur_timestamp.tm_min), fill="white")
+                if cur_timestamp.tm_min<10: #clock
+                    legacy.text(draw, (0, 0), str(cur_timestamp.tm_hour)+" 0"+str(cur_timestamp.tm_min), fill="white")
+                else:
+                    legacy.text(draw, (0, 0), str(cur_timestamp.tm_hour)+" "+str(cur_timestamp.tm_min), fill="white")
             
             if gLog:
                 try:
@@ -182,7 +223,7 @@ def displayData(pipe):
             
             try:
                 s="SF&MD"
-                legacy.text(draw, (d[0]-len(s)*8, 0), s, fill="white") #watermark
+                legacy.text(draw, (d[0]-len(s)*8, 0), "\3", fill="white", font=MY_CUSTOM_BITMAP_FONT) #watermark
             except:
                 time.sleep(0)
             
@@ -192,8 +233,9 @@ def displayData(pipe):
                 time.sleep(0)
             
             try:
-                legacy.text(draw, (0, 13), str(packet.lat), fill="white", font=legacy.font.TINY_FONT)
-                legacy.text(draw, (0, 18), str(packet.lon), fill="white", font=legacy.font.TINY_FONT)
+                legacy.text(draw, (0, 15), "Lat: "+str(packet.lat), fill="white", font=legacy.font.TINY_FONT)
+                legacy.text(draw, (0, 21), "Lon: "+str(packet.lon), fill="white", font=legacy.font.TINY_FONT)
+                legacy.text(draw, (0, 27), "Cycle: "+str(cycle), fill="white", font=legacy.font.TINY_FONT)
             except:
                 time.sleep(0)
             
@@ -210,124 +252,101 @@ def displayData(pipe):
                 legacy.text(draw, (0, d[1]-22), " ", fill="white", font=legacy.font.SINCLAIR_FONT)
             
             try: #speed as reported by gps
-                legacy.text(draw, (0, d[1]-14), "Speed: "+str(packet.hspeed), fill="white", font=legacy.font.SINCLAIR_FONT)
+                legacy.text(draw, (0, d[1]-14), "Speed: "+str(packet.hspeed)+" m/s", fill="white", font=legacy.font.SINCLAIR_FONT)
             except:
                 legacy.text(draw, (0, d[1]-14), "Speed: unknown", fill="white", font=legacy.font.SINCLAIR_FONT)
             
-            try: #address display
-                legacy.text(draw, (0, d[1]-6), road+" "+house, fill="white", font=legacy.font.TINY_FONT) #renders address on the bottom of display (TINY_FONT is 5x3 pixels).
-            except:
-                legacy.text(draw, (0, d[1]-6), "unknown address", fill="white", font=legacy.font.TINY_FONT)
-                print("DISPLAY: No address to display")
+            if gReverseGeocode:
+                try: #address display
+                    legacy.text(draw, (0, d[1]-6), road+" "+house, fill="white", font=legacy.font.TINY_FONT) #renders address on the bottom of display (TINY_FONT is 5x3 pixels).
+                except:
+                    legacy.text(draw, (0, d[1]-6), "unknown address", fill="white", font=legacy.font.TINY_FONT)
+                    print("DISPLAY: No address to display")
             #print(reverse_geocode_result.displayName())
 
-def main(receivedGeo, sentData):
+def processingData(sentData):
     global csv_file
     global csv_file_debug
     global writer
     global writer_debug
-    global road
-    global house
+    
+    #global reverse_geocode_result
     init()
     
     while True:
-        try:
-            cur_time=time.time()
-            date_diff=cur_time-start_time #save current loop timestamp in relation to starting time
-            
-            if date_diff>=gRecording_time and gLog and stop_after_time_passed:
-               csv_file.close()
-               exit()
-            #print("Temp : "+str(mpu.get_temp())) #thermometer module (unused)
-            
-            #get gps position
-            try:
-                packet = gpsResolve()
-            except:
-                print("MAIN: Can't resolve GPS")
-            
-            #resolve reverse geocoded address
-            if gReverseGeocode:
-                try:
-                    #if receivedGeo.poll(): #if there's data in the geocoding pipe
-                        #reverse_geocode_result = receivedGeo.recv()
-                    time.sleep(0)
-                except:
-                    print("MAIN: Can't resolve address")
-            
-            oldroad=road
-            oldhouse=house
-            
-            #fallback to old address if reverse geocoding failed
-            try:
-                #legacy.text(draw, (0, d[1]-8), reverse_geocode_result.address()['road']+" "+reverse_geocode_result.address()['house_number'], fill="white", font=legacy.font.TINY_FONT)
-                #print(reverse_geocode_result.displayName())
-                
-                road = reverse_geocode_result.address()['road']
-                
-                house = reverse_geocode_result.address()['house_number']
-                print("MAIN: "+road+" "+house)
-            except:
-                house = oldhouse
-                road = oldroad
-                print("MAIN: Old: "+road+" "+house)
-            
-            #accelerometer data acquisition
-            try:
-                accel_data = mpu.get_accel_data() 
-                ax = accel_data['x']
-                ay = accel_data['y']
-                az = accel_data['z'] #save accelerometer elements to variables
-                
-                a_length=math.sqrt(ax**2+ay**2+az**2) #calculate [ax, ay, az] length (slightly faster than numpy)
-            except:
-                print("MAIN: Can't resolve accelerometer data") #literally never happened
-            
-            if gLog:
-                try:
-                    writer.writerow([date_diff, ax, ay, az, a_length, packet.lat, packet.lon, packet.hspeed, road, house])
-                except:
-                    print("MAIN: Couldn't write to log file")
-            
-            newsleep=Tp-((time.time()-start_time)-date_diff)
-            
-            if gDebug:
-                try:
-                    writer_debug.writerow([time.time(),date_diff,time.time()-start_time,newsleep])
-                except:
-                    print("MAIN: Couldn't write to debug file")
-            
-            try:
-                sentData.send([a_length, packet, road, house])
-            except:
-                print("MAIN: Couldn't send data")
-            
-            if newsleep>0:
-                time.sleep(newsleep)
-                print('MAIN: something happened')
+        cur_time=time.time()
+        date_diff=cur_time-start_time #save current loop timestamp in relation to starting time
         
-        except KeyboardInterrupt: #this is important. without it, the processes might run indefinitely.
-            geocode_process.terminate()
-            display_process.terminate()
-            main_process.terminate()
+        if date_diff>=gRecording_time and gLog and stop_after_time_passed:
+           csv_file.close()
+           exit()
+        #print("Temp : "+str(mpu.get_temp())) #thermometer module (unused)
+        
+        #get gps position
+        try:
+            packet = gpsResolve()
+        except:
+            print("MAIN: Can't resolve GPS")
+        
+        #moved road and house to displaydata - 06.09.21
+        
+        #accelerometer data acquisition
+        try:
+            accel_data = mpu.get_accel_data() 
+            ax = accel_data['x']
+            ay = accel_data['y']
+            az = accel_data['z'] #save accelerometer elements to variables
+            
+            a_length=math.sqrt(ax**2+ay**2+az**2) #calculate [ax, ay, az] length (slightly faster than numpy)
+        except:
+            print("MAIN: Can't resolve accelerometer data") #literally never happened
+        
+        if gLog:
+            try:
+                writer.writerow([date_diff, ax, ay, az, a_length, packet.lat, packet.lon, packet.hspeed])
+            except:
+                print("MAIN: Couldn't write to log file")
+        
+        cycle=(time.time()-start_time)-date_diff #this calculates current cycle length
+        
+        if gDebug:
+            try:
+                writer_debug.writerow([time.time(),date_diff,time.time()-start_time,newsleep])
+            except:
+                print("MAIN: Couldn't write to debug file")
+        
+        try:
+            sentData.send([a_length, packet, cycle])
+        except KeyboardInterrupt: #this is important. without it, the display process would run indefinitely.
             raise SystemExit
+        except:
+            print("MAIN: Couldn't send data to display")
+        
+        if cycle<Tp:
+            time.sleep(Tp-cycle) #approximately allow for regular cycle times (alike RTC system)
+        else:
+            print('MAIN: Cycle time too long.')
+            
 
 
 if __name__ == "__main__":
     mp.set_start_method('fork')
     receivedData, sentData = mp.Pipe()
-    receivedGeo, sentGeo = mp.Pipe()
-    main_process = mp.Process(target = main, args=((receivedGeo, sentData),))
-    display_process = mp.Process(target = displayData, args=((receivedData, sentData),))
-    geocode_process = mp.Process(target = reverseGeocode, args=((sentGeo),))
-    
+    main_process = mp.Process(target = processingData, args=((sentData),))
+    display_process = mp.Process(target = displayData, args=((receivedData),))
+    #geocode_process = mp.Process(target = reverseGeocode) #instead calling the function in displayData. - 06.09.21
+    #it needs to be in its own process - sometimes hangs the display.
     print("Starting main process.")
     main_process.start()
     
     if gDisplayData:
         print("Starting display process.")
-        display_process.start() #works with data exchange - 06.09.21
-    
-    if gReverseGeocode: #TODO
-        print("Starting reverse geocode process.")
-        geocode_process.start()
+        display_process.start()
+     
+    while True:
+        try:
+            time.sleep(0)
+        except KeyboardInterrupt: #this is important. without it, the display process would run indefinitely.
+            display_process.terminate()
+            main_process.terminate()
+            raise SystemExit
